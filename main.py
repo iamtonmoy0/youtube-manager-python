91185b407d8d88d497fe17f9ed16224dec56f690
iamtonmoy0 <tonmoy2code@gmail.com>
2024-03-07 23:53:30 +0600

    Refactored code to use functions for loading data and the main loop.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+def load_data():
+    pass
+
+
 def all_videos(videos):
     pass
 
@@ -14,26 +18,31 @@ def delete_video(videos):
     pass
 
 
-videos = []
-while True:
-    print("\nWelcome to Youtube Manager")
-    print("1. List All Youtube Videos")
-    print("2. Add a Youtube Video")
-    print("3. Update Youtube Video Details")
-    print("4. Delete a Youtube Video")
-    print("5. Exit From App")
-    choice = input("Enter Your Choice: ")
+def main():
+    videos = load_data()
+    while True:
+        print("\nWelcome to Youtube Manager")
+        print("1. List All Youtube Videos")
+        print("2. Add a Youtube Video")
+        print("3. Update Youtube Video Details")
+        print("4. Delete a Youtube Video")
+        print("5. Exit From App")
+        choice = input("Enter Your Choice: ")
 
-    match choice:
-        case "1":
-            all_videos(videos)
-        case "2":
-            add_video(videos)
-        case "3":
-            update_video(videos)
-        case "4":
-            delete_video(videos)
-        case "5":
-            break
-        case _:
-            print("Invalid Choice")
+        match choice:
+            case "1":
+                all_videos(videos)
+            case "2":
+                add_video(videos)
+            case "3":
+                update_video(videos)
+            case "4":
+                delete_video(videos)
+            case "5":
+                break
+            case _:
+                print("Invalid Choice")
+
+
+if __name__ == "__main__":
+    main()
